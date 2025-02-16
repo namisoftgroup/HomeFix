9aa0b9d6472c357904e08a00ba7c92ac66300d51
Mariam Samir <ms1143440@gmail.com>
2025-02-16 15:28:02 +0200

    up

--- src/ui/cards/OrderCard.jsx
+++ src/ui/cards/OrderCard.jsx
@@ -1,32 +1,33 @@
+import { Link } from "react-router-dom";
+
 const OrderCard = ({ order, isPrevious }) => {
-    return (
-      <div className="order-card">
-        <div className="order-header">
+  return (
+    <div className="order-card">
+      <div className="order-header">
         <div className="service-icon">
           <img src={order.icon} alt={order.service} />
           <span>{order.service}</span>
         </div>
-          <span className="details">التفاصيل</span>
-
-        </div>
-        <div className="order-content">
-          <div className="order-info">
-            <p>
-              <i className="fa-solid fa-location-dot"></i> {order.location}
-            </p>
-            <p>
-              <i className="fa-regular fa-calendar"></i> {order.date}
-            </p>
-            <p>
-              <i className="fa-regular fa-clock"></i> {order.time}
-            </p>
-          </div>
-
-        </div>
-        {isPrevious && <button className="cancel-btn">طلب ملغي</button>}
+        <Link to={`/order-details?id=${order.id}`} className="details">
+          التفاصيل
+        </Link>
       </div>
-    );
-  };
-  
-  export default OrderCard;
-  
+      <div className="order-content">
+        <div className="order-info">
+          <p>
+            <i className="fa-solid fa-location-dot"></i> {order.location}
+          </p>
+          <p>
+            <i className="fa-regular fa-calendar"></i> {order.date}
+          </p>
+          <p>
+            <i className="fa-regular fa-clock"></i> {order.time}
+          </p>
+        </div>
+      </div>
+      {isPrevious && <button className="cancel-btn">طلب ملغي</button>}
+    </div>
+  );
+};
+
+export default OrderCard;
