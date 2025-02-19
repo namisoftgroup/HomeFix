89e8834c9fd331113843d4194b00f0bebaa1fbf0
Mahmoud Elkomy <mahmoudelkomy996@gmail.com>
2025-02-19 15:54:48 +0200

    edit

--- src/ui/orders/OrderTimeLine.jsx
+++ src/ui/orders/OrderTimeLine.jsx
@@ -20,6 +20,53 @@ export default function OrderTimeLine({ orderDetails }) {
           <img src="/icons/phone-fill.svg" alt="" />
         </Link>
       </div>
+
+      <div className="providerTimeLine">
+        <div className="step done">
+          <div className="statusLine">
+            <div className="icon">
+              <img src="/public/icons/check.svg" alt="" />
+            </div>
+          </div>
+          <div className="content">
+            <h6 className="title"> تم تقديم عرض سعر   <span className="price"> 500 <small> دينار </small> </span> </h6>
+            <p className="dateTime"> <span> 10:24م </span> <span> 20/01/2025 </span> </p>
+          </div>
+        </div>
+        <div className="step">
+          <div className="statusLine">
+            <div className="icon">
+              <img src="/public/icons/wait.svg" alt="" />
+            </div>
+          </div>
+          <div className="content">
+            <h6 className="title"> تم تقديم عرض سعر   <span className="price"> 500 <small> دينار </small> </span> </h6>
+            <p className="dateTime"> <span> 10:24م </span> <span> 20/01/2025 </span> </p>
+          </div>
+        </div>
+        <div className="step">
+          <div className="statusLine">
+            <div className="icon">
+              <img src="/public/icons/wait.svg" alt="" />
+            </div>
+          </div>
+          <div className="content">
+            <h6 className="title"> تم تقديم عرض سعر   <span className="price"> 500 <small> دينار </small> </span> </h6>
+            <p className="dateTime"> <span> 10:24م </span> <span> 20/01/2025 </span> </p>
+          </div>
+        </div>
+        <div className="step">
+          <div className="statusLine">
+            <div className="icon">
+              <img src="/public/icons/wait.svg" alt="" />
+            </div>
+          </div>
+          <div className="content">
+            <h6 className="title"> تم تقديم عرض سعر   <span className="price"> 500 <small> دينار </small> </span> </h6>
+            <p className="dateTime"> <span> 10:24م </span> <span> 20/01/2025 </span> </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
