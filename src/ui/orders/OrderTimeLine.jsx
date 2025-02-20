7dea86dab682323d5df42e9bd6cce97909c41e3a
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2025-02-20 08:53:58 +0000

    Add confirmation and finish buttons in OrderTimeLine; update styles and localization for new actions

--- src/ui/orders/OrderTimeLine.jsx
+++ src/ui/orders/OrderTimeLine.jsx
@@ -1,8 +1,14 @@
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
+import { toast } from "sonner";
+import { useQueryClient } from "@tanstack/react-query";
+import SubmitButton from "./../form-elements/SubmitButton";
+import useChangeOrderStatus from "../../hooks/orders/useChangeOrderStatus";
 
 export default function OrderTimeLine({ orderDetails }) {
   const { t } = useTranslation();
+  const queryClient = useQueryClient();
+  const { isPending, changeOrderStatus } = useChangeOrderStatus();
 
   const historyStatus = [
     "new",
@@ -17,6 +23,32 @@ export default function OrderTimeLine({ orderDetails }) {
     "set_images",
     "complete",
   ];
+
+  const handleConfirmArrival = () => {
+    changeOrderStatus(
+      {
+        orderId: orderDetails?.id,
+        request: {
+          status: "confirm_arrival",
+        },
+      },
+      {
+        onSuccess: (res) => {
+          if (res?.code === 200) {
+            toast.success(res?.message);
+            queryClient.invalidateQueries(["orders"]);
+            queryClient.invalidateQueries(["order-details", orderDetails?.id]);
+          } else {
+            toast.error(res?.message);
+          }
+        },
+        onError: (err) => {
+          console.log(err);
+          toast.error("Some thing went wrong, please try again or contact us.");
+        },
+      }
+    );
+  };
 
   return (
     <>
@@ -79,6 +111,27 @@ export default function OrderTimeLine({ orderDetails }) {
                     <span>{orderDetails?.order_status_history?.[status]}</span>
                   </p>
                 )}
+
+                {orderDetails?.status === "accept" &&
+                  status === "confirm_arrival" && (
+                    <div className="form">
+                      <SubmitButton
+                        name={t("confirmArrival")}
+                        onClick={handleConfirmArrival}
+                        loading={isPending}
+                      />
+                    </div>
+                  )}
+
+                {orderDetails?.status === "confirm_arrival" &&
+                  status === "confirm_items" && (
+                    <div className="form">
+                      <div className="d-flex gap-2">
+                        <button className="finish">{t("finish")}</button>
+                        <SubmitButton name={t("add")} />
+                      </div>
+                    </div>
+                  )}
               </div>
             )}
           </div>
