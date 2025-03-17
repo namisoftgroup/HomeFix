7ba1acffd61c988be2f21037f9f5e05ffa1cd997
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2025-03-17 11:43:16 +0200

    Add user and technical app sections to GetApp modal; update localization and styles for improved layout

--- src/ui/modals/GetApp.jsx
+++ src/ui/modals/GetApp.jsx
@@ -12,26 +12,49 @@ export default function GetApp({ show, setShow }) {
       className="getAppModal"
     >
       <Modal.Body>
-        <h4 className="modalTitle">
+        <h4 className="modalTitle mb-3">
           {t("getApp.title")} <span> {t("appName")} </span>
         </h4>
-        <h6 className="subtitle"> {t("getApp.subtitle")} </h6>
-        <img className="qrcode" src="/icons/qr.png" alt="" />
-        <div className="btns">
-          <Link
-            aria-label="Apple App"
-            to="https://apps.apple.com/us/app/home-fix-technical/id6740774757"
-            target="_blank"
-          >
-            <img src="/icons/appStore.svg" alt="" />
-          </Link>
-          <Link
-            aria-label="Andorid App"
-            to="https://play.google.com/store/apps/details?id=com.apps.homefix"
-            target="_blank"
-          >
-            <img src="/icons/playStore.svg" alt="" />
-          </Link>
+        <div className="d-flex align-items-center justify-content-center gap-3">
+          <div className="app_container">
+            <h6 className="mb-3">{t("getApp.technicalApp")}</h6>
+            <div className="btns">
+              <Link
+                aria-label="Apple App"
+                to="https://apps.apple.com/us/app/home-fix-technical/id6740774757"
+                target="_blank"
+              >
+                <img src="/icons/appStore.svg" alt="" />
+              </Link>
+              <Link
+                aria-label="Andorid App"
+                to="https://play.google.com/store/apps/details?id=com.apps.homefixtechnical"
+                target="_blank"
+              >
+                <img src="/icons/playStore.svg" alt="" />
+              </Link>
+            </div>
+          </div>
+
+          <div className="app_container">
+            <h6 className="mb-3">{t("getApp.userApp")}</h6>
+            <div className="btns">
+              <Link
+                aria-label="Apple App"
+                to="https://apps.apple.com/us/app/home-fix/id6740774660"
+                target="_blank"
+              >
+                <img src="/icons/appStore.svg" alt="" />
+              </Link>
+              <Link
+                aria-label="Andorid App"
+                to="https://play.google.com/store/apps/details?id=com.apps.homefix"
+                target="_blank"
+              >
+                <img src="/icons/playStore.svg" alt="" />
+              </Link>
+            </div>
+          </div>
         </div>
       </Modal.Body>
     </Modal>
